72021ff47100497df9ef2a7cfc1da95dd881e70d
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2025-06-01 18:16:07 +0330

    add colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,30 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="p-4">
-        <div className="font-bold text-h1">Hot Dishes</div>
-        <div className="font-bold text-h2">Hot Dishes</div>
-        <div className="font-bold text-body-large-regular">Hot Dishes</div>
-        <div className="font-bold text-body-large-medium">Hot Dishes</div>
-        <div className="font-bold text-body-large-semibold">Hot Dishes</div>
-        <div className="font-bold text-body-normal-medium">Hot Dishes</div>
+      <div className="p-4 drop-shadow-custom-1">
+        <div className="font-bold text-h1 text-primary">Hot Dishes</div>
+        <div className="font-bold text-h2 text-secondary">Hot Dishes</div>
+        <div className="font-bold text-body-large-regular text-accent-green">
+          Hot Dishes
+        </div>
+        <div className="font-bold text-body-large-medium text-accent-orange">
+          Hot Dishes
+        </div>
+        <div className="font-bold text-body-large-semibold text-accent-red">
+          Hot Dishes
+        </div>
+        <div className="font-bold text-body-normal-medium text-dark-text">
+          Hot Dishes
+        </div>
         <div className="font-bold text-body-normal-regular">Hot Dishes</div>
         <div className="font-bold text-body-normal-semibold">Hot Dishes</div>
         <div className="font-bold text-body-small-medium">Hot Dishes</div>
         <div className="font-bold text-body-small-regular">Hot Dishes</div>
         <div className="font-bold text-button-normal-semibold">Hot Dishes</div>
         <div className="font-bold text-button-small-semibold">Hot Dishes</div>
+        <button className="flex items-center justify-center bg-primary py-3 px-10 rounded text-white cursor-pointer text-button-normal-semibold">
+          buy pizza
+        </button>
       </div>
     </>
   );
