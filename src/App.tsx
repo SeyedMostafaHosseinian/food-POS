89e2fe2fc6607bbd7b1fdff7857fb67496ea0547
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2025-06-01 17:19:36 +0330

    config custome font size utilities

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="font-bold">Hot Dishes</div>
+      <div className="p-4">
+        <div className="font-bold text-h1">Hot Dishes</div>
+        <div className="font-bold text-h2">Hot Dishes</div>
+        <div className="font-bold text-body-large-regular">Hot Dishes</div>
+        <div className="font-bold text-body-large-medium">Hot Dishes</div>
+        <div className="font-bold text-body-large-semibold">Hot Dishes</div>
+        <div className="font-bold text-body-normal-medium">Hot Dishes</div>
+        <div className="font-bold text-body-normal-regular">Hot Dishes</div>
+        <div className="font-bold text-body-normal-semibold">Hot Dishes</div>
+        <div className="font-bold text-body-small-medium">Hot Dishes</div>
+        <div className="font-bold text-body-small-regular">Hot Dishes</div>
+        <div className="font-bold text-button-normal-semibold">Hot Dishes</div>
+        <div className="font-bold text-button-small-semibold">Hot Dishes</div>
+      </div>
     </>
   );
 }
