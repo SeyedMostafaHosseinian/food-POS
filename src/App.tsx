b2cb869f0c9a7020013b2e90b1991774871889aa
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2025-06-02 18:29:19 +0330

    feat - add base button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Button from "./components/base/Button";
+import Icon from "./components/Icon";
 import IconRegistry from "./components/IconRegistry";
 import SideNav from "./components/SideNav";
 
@@ -7,6 +9,29 @@ function App() {
     <main className="w-full h-screen dark:bg-dark-bg-1 bg-light-bg-1">
       <IconRegistry />
       <SideNav />
+      <div className="ms-32 p-4">
+        <Button className="w-40">order</Button>
+        <Button className="w-40 m-2" variant="outline">
+          order
+        </Button>
+        <Button className="m-2 w-52" variant="outline">
+          delete
+          <Icon id="trash" />
+        </Button>
+        <Button className="m-2 w-52" variant="fill">
+          delete
+          <Icon id="trash" />
+        </Button>
+        <Button className="m-2 w-52" justIcon>
+          <Icon id="trash" />
+        </Button>
+        <Button className="m-2 w-52" variant="outline" justIcon>
+          <Icon id="trash" />
+        </Button>
+        <Button className="m-2 w-52" variant="outline" justIcon>
+          <Icon id="plus" />
+        </Button>
+      </div>
     </main>
   );
 }
---
+++ src/components/base/Button.tsx
@@ -0,0 +1,37 @@
+import type { ButtonHTMLAttributes } from "react";
+
+type Variant = "fill" | "outline";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Variant;
+  justIcon?: boolean;
+}
+
+export default function Button({
+  children,
+  variant = "fill",
+  className,
+  justIcon,
+  ...props
+}: ButtonProps) {
+  const variantClassNames: Record<Variant, string> = {
+    outline: "border border-primary text-primary",
+    fill: "bg-primary text-white",
+  };
+
+  return (
+    <button
+      {...props}
+      data-variant={variant}
+      data-just-icon={justIcon}
+      className={`rounded-lg flex max-h-12 items-center justify-center 
+        text-button-normal-semibold py-3.5 cursor-pointer hover:opacity-80 active:opacity-50
+        transition-opacity 
+        ${variantClassNames[variant]} ${className} ${
+        justIcon ? "px-3.5 max-w-12 *:w-5 *:h-5 *:shrink-0" : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
